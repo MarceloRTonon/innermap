f50442e5a72481c3b6f052907754bbf03db6abbb
MarceloRTonon <marceloresendetonon@gmail.com>
2020-10-14 15:01:46 -0300

    innermap family

---
+++ R/innermap_chr.R
@@ -0,0 +1,8 @@
+innermap_chr <- function(input, .f0){
+  require(rlang, warn.conflicts = F)
+  require(purrr)
+  .output <- map2_chr(c(1:(length(input)-1)), c(2:length(input)),
+                  function(x1,x2) rlang::exec(.f0, input[x1], input[x2])
+  )
+  return(.output)
+}
---
+++ R/innermap_int.R
@@ -0,0 +1,8 @@
+innermap_int <- function(input, .f0){
+  require(rlang, warn.conflicts = F)
+  require(purrr)
+  .output <- map2_int(c(1:(length(input)-1)), c(2:length(input)),
+                  function(x1,x2) rlang::exec(.f0, input[x1], input[x2])
+  )
+  return(.output)
+}
