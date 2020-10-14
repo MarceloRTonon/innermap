e49e5cd3a9cc74955ded69d51f3d5b5afbed5946
MarceloRTonon <marceloresendetonon@gmail.com>
2020-10-14 15:10:41 -0300

    adding the distance argument

--- R/innermap_dfr.R
+++ R/innermap_dfr.R
@@ -1,7 +1,7 @@
-innermap_dfr <- function(input, .f0){
+innermap_dfr <- function(input, .f0, distance = 1){
   require(rlang, warn.conflicts = F)
   require(purrr)
-  .output <- map2_dfr(c(1:(length(input)-1)), c(2:length(input)),
+  .output <- map2_dfr(c(1:(length(input)-distance)), c((1+distance):length(input)),
                   function(x1,x2) rlang::exec(.f0, input[x1], input[x2])
   )
   return(.output)
